392522fca5c1eb4dc3152cb8792ddd2e0aaa11ed
Jassi2201 <dbmongo080@gmail.com>
2026-06-05 14:38:04 +0530

    sdsss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <Route path="/admin/post" element={<AdminLayout><Posts/></AdminLayout>} />
          <Route path="/admin/quiz" element={<AdminLayout><Quiz/></AdminLayout>} />
          <Route path="/admin/cms" element={<AdminLayout><CMS/></AdminLayout>} />
-          <Route path="/admin/performanceAnalytics" element={<AdminLayout><PerformanceAnalytics/></AdminLayout>} />
+          {/* <Route path="/admin/performanceAnalytics" element={<AdminLayout><PerformanceAnalytics/></AdminLayout>} /> */}
         {/* Redirect any unknown routes to home */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
