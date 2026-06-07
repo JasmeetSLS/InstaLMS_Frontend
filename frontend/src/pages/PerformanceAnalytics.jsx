8cfb57f46d0e78358a4275a6f2c2b5ad92c6740e
Jassi2201 <dbmongo080@gmail.com>
2026-06-07 12:17:14 +0530

    sdds

--- frontend/src/pages/PerformanceAnalytics.jsx
+++ frontend/src/pages/PerformanceAnalytics.jsx
@@ -4,7 +4,7 @@ import HighchartsReactOfficial from "highcharts-react-official";
 
 const HighchartsReact = HighchartsReactOfficial.default || HighchartsReactOfficial;
 
-import mapModule from "highcharts/modules/map.js";
+// import mapModule from "highcharts/modules/map.js";
 
 import mapDataIndia from "@highcharts/map-collection/countries/in/in-all.geo.json";
 
